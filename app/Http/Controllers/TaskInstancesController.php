e00ff6194334096713c5e1971f60ecf30bd1caab
Dee Barizo <deebarizo@gmail.com>
2017-03-11 01:17:14 +0000

    Stores task and shows it on index view

---
+++ app/Http/Controllers/TaskInstancesController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Input;
+
+use App\UseCases\DateCalculator;
+use App\UseCases\FileUploader;
+
+use App\Models\Option;
+use App\Models\TaskInstance;
+
+use DB;
+
+class TaskInstancesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $h2Tag = 'Tasks'; 
+
+        $option = Option::find(1);
+
+        $dateCalculator = new DateCalculator;
+        $currentDate = $dateCalculator->getCurrentDate($option, $date = new \DateTime());
+
+        $tasks = TaskInstance::select('*')->where('date', $currentDate->format('Y-m-d'))->get();
+
+        # ddAll($tasks);
+
+        return view('tasks/index', compact('h2Tag', 'tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $h2Tag = 'Create Task';
+
+        return view('tasks/create', compact('h2Tag'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            
+            'name' => 'required',
+            'image' => 'required'
+        ]);
+
+        $taskInstance = new TaskInstance;
+
+        $taskInstance->name = trim($request->input('name'));
+        $taskInstance->description = (trim($request->input('description')) == '' ? null : trim($request->input('description')));
+        $taskInstance->link = (trim($request->input('link')) == '' ? null : trim($request->input('link')));
+
+        $taskInstance->save();
+ 
+        $fileUploader = new FileUploader;
+
+        $imageUrl = $fileUploader->uploadImageFile($request, 'task', $taskInstance->id);
+
+        $taskInstance->image_url = $imageUrl;
+
+        $option = Option::find(1);
+
+        $dateCalculator = new DateCalculator;
+        $currentDate = $dateCalculator->getCurrentDate($option, $date = new \DateTime());
+        $taskInstance->date = $currentDate->format('Y-m-d');
+
+        $taskInstance->start_time = $option->start_time;
+        $taskInstance->end_time = $option->end_time;
+        $taskInstance->is_complete = 0;
+        $taskInstance->completed_at = null;
+
+        $taskInstance->save();
+
+        return redirect()->route('tasks.index')->with('message', 'Success!'); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,142 @@
+@extends('master')
+
+@section('content')
+
+	@include('_form_heading')
+	
+	<div class="row">
+
+		<div class="col-lg-12">
+
+			<p style="margin-bottom: 20px"><a href="/tasks/create">Create Task</a></p>
+
+		</div>
+
+		@foreach ($tasks as $task)
+
+			<?php 
+
+				$isComplete = ($task->is_complete ? 'complete' : '');
+				$description = ($task->description == '' ? '' : '<a class="description" href="#"><img src="'.url('/files/icons/text-lines.png').'"></a><div style="display: none" class="tool-tip-description">'.$task->description.'</div>');
+				$link = ($task->link == '' ? '' : '<a target="_blank" href="'.$task->link.'"><img src="'.url('/files/icons/link.png').'"></a>');
+			
+			?>
+
+			<div class="col-lg-4">
+
+				<div class="task {{ $isComplete }}" style="height: 250px; border: 1px solid; margin-bottom: 30px" data-task-instance-id="{{ $task->id }}">
+					<h4 class="text-center" style="margin: 18px 18px">{{ $task->name }} {!! $description !!} {!! $link !!}</h4>
+
+					<img class="center-block" style="margin-bottom: 27px" src="<?php echo url($task->image_url); ?>">
+
+					<div class="text-center"><a href="#"><img src="<?php echo url('/files/icons/target.png'); ?>"></a> <a href="/daily_tasks/{{ $task->id }}/edit"><img src="<?php echo url('/files/icons/edit.png'); ?>"></a> <a class="complete" href="#"><img src="<?php echo url('/files/icons/checked.png'); ?>"></a> <form style="display: inline" method="POST" action="/daily_tasks/{{ $task->id}}" accept-charset="UTF-8" id="form-delete-daily-tasks-{{ $task->id }}"><input name="_method" type="hidden" value="DELETE">{{ csrf_field() }}<a class="data-delete" data-form="daily-tasks-{{ $task->id }}" href="#"><img src="<?php echo url('/files/icons/trash.png'); ?>"></a></form></div>
+				</div>
+
+			</div>
+
+		@endforeach
+
+	</div>
+
+	<script type="text/javascript">
+
+		$(document).ready(function() {
+
+
+			/****************************************************************************************
+			GLOBAL VARIABLES
+			****************************************************************************************/
+
+			var baseUrl = '<?php echo url('/'); ?>';
+
+
+			/****************************************************************************************
+			TOOLTIPS (DESCRIPTIONS)
+			****************************************************************************************/
+
+			$('a.description').on('mouseenter', function(event) {
+
+		        $(this).qtip({
+		        
+		            content: {
+		        
+		                text: $(this).next('.tool-tip-description')
+					},
+
+					position: {
+
+						my: 'left center',
+						at: 'center right',
+						target: $(this)
+					},
+					overwrite: false,
+		            show: {
+		                event: event.type,
+		                ready: true
+		            }
+		        });
+			});
+
+
+			/****************************************************************************************
+			AJAX SETUP
+			****************************************************************************************/
+
+			$.ajaxSetup({ // http://stackoverflow.com/a/37663496/1946525
+			    
+			    headers: {
+			        
+			        'X-CSRF-Token': $('input[name="_token"]').val()
+			    }
+			});
+
+
+			/****************************************************************************************
+			COMPLETING A TASK
+			****************************************************************************************/
+
+			$('a.complete').on('click', function(e) {
+
+				e.preventDefault(e);
+
+				var task = $(this).closest('div.task');
+				var taskInstanceId = task.attr('data-task-instance-id');
+				var isComplete = !task.hasClass('complete');
+
+
+				$.ajax({
+
+		            url: baseUrl+'/tasks/complete',
+		           	type: 'POST',
+		           	data: { 
+		           	
+		           		taskInstanceId: taskInstanceId,
+		           		isComplete: isComplete
+		           	},
+		            success: function() {
+
+		            	isComplete ? task.addClass('complete') : task.removeClass('complete');
+		            }
+		        });
+			})
+
+
+			/****************************************************************************************
+			DELETING A DAILY TASK
+			****************************************************************************************/
+
+			$(function () { // http://laraveldaily.com/resource-controller-delete-how-to-have-link-instead-of-a-submit-button/
+
+				$('.data-delete').on('click', function(e) {
+			    
+			    	if (!confirm('Are you sure you want to delete?')) return;
+			    
+			    		e.preventDefault();
+			    
+			    	$('#form-delete-'+$(this).data('form')).submit();
+			  	});
+			});
+		});		
+
+	</script>
+@stop
