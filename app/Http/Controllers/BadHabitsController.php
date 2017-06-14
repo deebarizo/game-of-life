f6e68efd3d17c4ec4ae385d7742862b32a48b79e
Dee Barizo <deebarizo@gmail.com>
2017-06-14 09:25:52 +0000

    Create bad_habits table

---
+++ app/Http/Controllers/BadHabitsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BadHabit;
+use Illuminate\Http\Request;
+
+class BadHabitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BadHabit  $badHabit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BadHabit $badHabit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BadHabit  $badHabit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BadHabit $badHabit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BadHabit  $badHabit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BadHabit $badHabit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BadHabit  $badHabit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BadHabit $badHabit)
+    {
+        //
+    }
+}
